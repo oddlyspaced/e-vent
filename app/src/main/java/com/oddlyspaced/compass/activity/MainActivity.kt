90975a066d5bde7f2e14735c6628c2da2baa1c37
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 09:13:39 +0530

    MainActivity: Start AccountActivity

--- app/src/main/java/com/oddlyspaced/compass/activity/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.oddlyspaced.compass.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.compass.R
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(applicationContext, AccountActivity::class.java))
     }
 }
