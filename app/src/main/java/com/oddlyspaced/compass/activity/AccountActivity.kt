d0cc3acee036d4368b2b3656a5fe6af88c96e8a6
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 09:12:48 +0530

    AccountActivity: Inflate AskNumFragment

--- app/src/main/java/com/oddlyspaced/compass/activity/AccountActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/activity/AccountActivity.kt
@@ -3,10 +3,21 @@ package com.oddlyspaced.compass.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.compass.R
+import com.oddlyspaced.compass.databinding.ActivityAccountBinding
+import com.oddlyspaced.compass.fragment.AskNumFragment
 
 class AccountActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account)
+        binding = ActivityAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragment = AskNumFragment.newInstance().apply {
+            root = binding.frameAccount.id
+        }
+        supportFragmentManager.beginTransaction().replace(binding.frameAccount.id, fragment).addToBackStack("AskNum").commit()
     }
 }
