695270f0b9670bcf65b32319a7b784254e9bd7f0
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 07:32:43 +0530

    DashboardActivity: Add view pager for fragments

--- app/src/main/java/com/oddlyspaced/compass/DashboardActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/DashboardActivity.kt
@@ -2,9 +2,13 @@ package com.oddlyspaced.compass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.oddlyspaced.compass.custom.CustomTabsAdapter
 import com.oddlyspaced.compass.databinding.ActivityDashboardBinding
+import com.oddlyspaced.compass.fragment.OverviewFragment
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -18,5 +22,12 @@ class DashboardActivity : AppCompatActivity() {
         val adapter = CustomTabsAdapter(mutableListOf("Overview", "Following", "Events", "Events", "events", "events"))
         binding.rvTabs.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
         binding.rvTabs.adapter = adapter
+
+        binding.viewPagerDashboard.adapter = ViewPagerAdapter(arrayListOf(OverviewFragment.newInstance()), supportFragmentManager)
+    }
+
+    inner class ViewPagerAdapter(private val fragments: ArrayList<Fragment>, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment = fragments[position]
+        override fun getCount(): Int = fragments.size
     }
 }
