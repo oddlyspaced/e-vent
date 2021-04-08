1d2e790d1748ebe1104b162c8819747610c3eb3e
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 08:32:36 +0530

    SearchFragment: Setup base

---
+++ app/src/main/java/com/oddlyspaced/compass/fragment/SearchFragment.kt
@@ -0,0 +1,35 @@
+package com.oddlyspaced.compass.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.oddlyspaced.compass.R
+import com.oddlyspaced.compass.custom.EventAdapter
+import com.oddlyspaced.compass.databinding.FragmentOverviewBinding
+import com.oddlyspaced.compass.databinding.FragmentSearchBinding
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+
+class SearchFragment: Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): SearchFragment {
+            return SearchFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSearchBinding.bind(LayoutInflater.from(context).inflate(R.layout.fragment_search, container, false))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.rvSearchEvents.layoutManager = LinearLayoutManager(context)
+        binding.rvSearchEvents.adapter = EventAdapter()
+    }
+}
