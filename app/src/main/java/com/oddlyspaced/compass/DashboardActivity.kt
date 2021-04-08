9e155bcb773f582558df903fe9df9c18353e138d
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 06:18:15 +0530

    DashboardActivity: Setup sample tab recycler

--- app/src/main/java/com/oddlyspaced/compass/DashboardActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/DashboardActivity.kt
@@ -2,10 +2,21 @@ package com.oddlyspaced.compass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.oddlyspaced.compass.custom.CustomTabsAdapter
+import com.oddlyspaced.compass.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = CustomTabsAdapter(mutableListOf("Overview", "Following", "Events", "Events", "events", "events"))
+        binding.rvTabs.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvTabs.adapter = adapter
     }
 }
