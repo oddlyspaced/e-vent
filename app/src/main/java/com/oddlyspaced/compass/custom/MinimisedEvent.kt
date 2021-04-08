788e2ad25678d90a247f54c23ff5c4ab9c8b0996
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 06:52:22 +0530

    MinimisedEvent: Setup base

---
+++ app/src/main/java/com/oddlyspaced/compass/custom/MinimisedEvent.kt
@@ -0,0 +1,51 @@
+package com.oddlyspaced.compass.custom
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.cardview.widget.CardView
+import com.oddlyspaced.compass.R
+import com.oddlyspaced.compass.databinding.LayoutCustomButtonBinding
+import com.oddlyspaced.compass.databinding.LayoutEventMinBinding
+import com.oddlyspaced.compass.util.DimensionUtils
+
+class MinimisedEvent: CardView {
+
+    private lateinit var binding: LayoutEventMinBinding
+
+    constructor(ctx: Context) : super(ctx) {
+        init(ctx)
+    }
+
+    constructor(ctx: Context, attr: AttributeSet) : super(ctx, attr) {
+        init(ctx)
+        setAttr(attr)
+    }
+
+    constructor(ctx: Context, attr: AttributeSet, defStyleAttr: Int) : super(ctx, attr, defStyleAttr) {
+        init(ctx)
+        setAttr(attr)
+    }
+
+    private fun init(context: Context) {
+        binding = LayoutEventMinBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.consEventMin.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        this.elevation = DimensionUtils.floatToDp(context, 2F)
+        this.radius = DimensionUtils.floatToDp(context, 12F)
+    }
+
+    private fun setAttr(attributes: AttributeSet) {
+//        val typedArray = context.obtainStyledAttributes(attributes, R.styleable.CustomButton, 0, 0)
+//        try {
+//            binding.txCustomButton.text = typedArray.getString(R.styleable.CustomButton_text)
+//        }
+//        catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//        finally {
+//            typedArray.recycle()
+//        }
+    }
+
+}
