3dcea663c954ce47896d3f835d233917fc062d91
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-08 07:31:52 +0530

    CustomViewPager: Copy over

---
+++ app/src/main/java/com/oddlyspaced/compass/custom/CustomViewPager.kt
@@ -0,0 +1,24 @@
+package com.oddlyspaced.compass.custom
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class CustomViewPager: ViewPager {
+
+    var isDisabled = false
+
+    constructor(context: Context): super(context)
+    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        return !isDisabled && super.onTouchEvent(ev)
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        return !isDisabled && super.onInterceptTouchEvent(ev)
+    }
+}
