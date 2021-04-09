21a95178516dc708694b6f92885d0ec1c27f87ea
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-09 20:11:43 +0530

    layout_tab, layout_tab_inactive, CustomTabsAdapter: Improve layout feel

--- app/src/main/java/com/oddlyspaced/compass/custom/CustomTabsAdapter.kt
+++ app/src/main/java/com/oddlyspaced/compass/custom/CustomTabsAdapter.kt
@@ -6,47 +6,60 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.oddlyspaced.compass.R
 import com.oddlyspaced.compass.databinding.LayoutTabBinding
+import com.oddlyspaced.compass.databinding.LayoutTabInactiveBinding
 
-class CustomTabsAdapter(private val items: MutableList<String>, private val onClickTab: (Int) -> Unit): RecyclerView.Adapter<CustomTabsAdapter.TabViewHolder>() {
+class CustomTabsAdapter(private val items: MutableList<String>, private val onClickTab: (Int) -> Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+
+    private companion object {
+        const val TYPE_ACTIVE = 1
+        const val TYPE_INACTIVE = 0
+    }
     var active = 0
         set(value) {
             field = value
             onClickTab(value)
-            items.forEachIndexed { index, _ ->
-                notifyItemChanged(index)
-            }
-            notifyItemChanged(value)
+            notifyDataSetChanged()
+//            items.forEachIndexed { index, _ ->
+//                notifyItemChanged(index)
+//            }
+//            notifyItemChanged(value)
         }
 
-    inner class TabViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class ActiveTabViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val binding = LayoutTabBinding.bind(itemView)
-        val card = binding.cvCustomTab
         val text = binding.txCustomTab
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TabViewHolder {
-        return TabViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_tab, parent, false))
+    inner class InactiveTabViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val binding = LayoutTabInactiveBinding.bind(itemView)
+        val text = binding.txCustomTab
     }
 
-    override fun onBindViewHolder(holder: TabViewHolder, position: Int) {
-        holder.card.setOnClickListener {
-            if (position != active) {
-                active = position
-            }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_ACTIVE -> ActiveTabViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_tab, parent, false))
+            TYPE_INACTIVE -> InactiveTabViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_tab_inactive, parent, false))
+            else -> throw Exception("Unidentified Tab Type!")
         }
-        holder.text.text = items[position]
-        holder.itemView.context.let { context ->
-            when(position) {
-                active -> {
-                    holder.card.setCardBackgroundColor(context.getColor(R.color.accent))
-                    holder.text.setTextColor(context.getColor(R.color.text_light))
-                }
-                else -> {
-                    // inactive
-                    holder.card.setCardBackgroundColor(context.getColor(R.color.background))
-                    holder.text.setTextColor(context.getColor(R.color.text_dark))
-                }
+    }
+
+    override fun getItemViewType(position: Int): Int = if (position == active) TYPE_ACTIVE else TYPE_INACTIVE
+
+    override fun onBindViewHolder(hol: RecyclerView.ViewHolder, position: Int) {
+        val item = items[position]
+        hol.itemView.setOnClickListener {
+            active = position
+            onClickTab(position)
+        }
+        when(hol.itemViewType) {
+            TYPE_ACTIVE -> {
+                val holder = hol as ActiveTabViewHolder
+                holder.text.text = item
+            }
+            TYPE_INACTIVE -> {
+                val holder = hol as InactiveTabViewHolder
+                holder.text.text = item
             }
         }
     }
