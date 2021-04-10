88b785a30ca5ca90918d69368b357d7453503700
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-10 13:46:57 +0530

    Global: Add sample item

--- app/src/main/java/com/oddlyspaced/compass/Global.kt
+++ app/src/main/java/com/oddlyspaced/compass/Global.kt
@@ -30,6 +30,30 @@ class Global {
                     )
                 )
             ),
+            EventItem(
+                2,
+                "IEEE",
+                "https://github.com/oddlyspaced/M-pal/blob/main/media/ieee.png?raw=true",
+                1000,
+                "MUJ Hacks",
+                "April 8th",
+                "Online",
+                true,
+                arrayListOf("tag1", "tag2"),
+                "banner link",
+                "IEEE S-CHAP MUJ brings you to the yearly Hackathon \"MUJ HACKS\".Compete with your team of 4 from 8th April to 11th April and drill your coding skills to come out as an innovator.\nThe event is free for all.\nCash prizes upto 1l akh rupees\n \nLast date to register is 7 th April so grab your pillows as you are going have some sleepless nights",
+                "https://res.cloudinary.com/dashed/image/upload/v1611051427/acm/klgjkuqdehb2g4buvprx.png",
+                arrayListOf(
+                    EventContact(
+                        "Contact 1",
+                        "9999997777",
+                    ),
+                    EventContact(
+                        "Contact 2",
+                        "87879787988"
+                    )
+                )
+            ),
         )
     }
 }
