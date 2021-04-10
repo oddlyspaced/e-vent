a45c64f2f5f9d7a36e0dcfcd9d8b4e8a959ff1de
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-10 05:43:23 +0530

    Global: Adapt data

--- app/src/main/java/com/oddlyspaced/compass/Global.kt
+++ app/src/main/java/com/oddlyspaced/compass/Global.kt
@@ -4,59 +4,69 @@ import com.oddlyspaced.compass.modal.EventItem
 
 class Global {
     companion object {
+
+        const val VALUE_TYPE_EVENT_INTER = 1
+        const val VALUE_TYPE_EVENT_INTRA = 2
+
         fun getMockData() = arrayListOf(
             EventItem(
                 1,
-                "Hackathon",
+                "Hackathon inter",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                false,
                 arrayListOf("Android")
             ),
             EventItem(
                 2,
-                "Study Jam",
+                "Study Jam inter",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                false,
                 arrayListOf("jam", "app")
             ),
             EventItem(
                 3,
-                "Workshop",
+                "Workshop inter",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                false,
                 arrayListOf("hacking", "web")
             ),
             EventItem(
                 4,
-                "Hackathon 123",
+                "Hackathon 123 intra",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                true,
                 arrayListOf("Android")
             ),
             EventItem(
                 5,
-                "Study Jam",
+                "Study Jam intra",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                true,
                 arrayListOf("jam", "app")
             ),
             EventItem(
                 6,
-                "Workshop",
+                "Worksho p intra",
                 "https://raw.githubusercontent.com/oddlyspaced/covid19-android/master/screenshots/icon.png",
                 "April 08 - 12:00 PM",
                 "Dome building",
                 1001,
+                true,
                 arrayListOf("hacking", "web")
             ),
         )
