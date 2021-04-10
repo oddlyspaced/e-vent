7007fe2c79889f059713f6b2f3186406ddbbf71b
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-10 11:57:10 +0530

    EventDetailActivity: Get string extras from intent to set data

--- app/src/main/java/com/oddlyspaced/compass/activity/EventDetailActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/activity/EventDetailActivity.kt
@@ -2,24 +2,49 @@ package com.oddlyspaced.compass.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.oddlyspaced.compass.R
+import com.oddlyspaced.compass.databinding.ActivityEventDetailBinding
+import java.lang.Exception
 
 class EventDetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val CLUB_LOGO = "club_logo"
+        const val CLUB_NAME = "club_name"
+        const val EVENT_NAME = "event_title"
+        const val EVENT_DATE = "event_date"
+        const val EVENT_LOCATION = "event_location"
+        const val EVENT_BANNER = "event_banner"
+        const val EVENT_DESC = "event_desc"
+        const val REGIS_LINK = "registration_link"
+     }
+
+    private lateinit var binding: ActivityEventDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_detail)
-    }
-}
-/*
-time
-location
-title
-description
-club
-facilitator
-registration link
-registration button
-cover image
+        binding = ActivityEventDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
--> Add to google calendar (automatic)
- */
+        init()
+    }
+
+    private fun init() {
+        try {
+            intent.run {
+                Glide.with(applicationContext).load(getStringExtra(CLUB_LOGO)).into(binding.imgDetailsLogo)
+                Glide.with(applicationContext).load(getStringExtra(EVENT_BANNER)).into(binding.imgDetailsBanner)
+                binding.txDetailsClubName.text = getStringExtra(CLUB_NAME)
+                binding.txDetailsTitle.text = getStringExtra(EVENT_NAME)
+                binding.txDetailsCalendar.text = getStringExtra(EVENT_DATE)
+                binding.txDetailsLocation.text = getStringExtra(EVENT_LOCATION)
+                binding.txDetailsDesc.text = getStringExtra(EVENT_DESC)
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
