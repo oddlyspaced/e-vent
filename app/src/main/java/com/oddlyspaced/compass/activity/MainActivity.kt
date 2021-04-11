968abbdbd1b47c7295c85efe5b7f63aedfc63ab7
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-11 09:53:16 +0530

    MainActivity: Setup splash

--- app/src/main/java/com/oddlyspaced/compass/activity/MainActivity.kt
+++ app/src/main/java/com/oddlyspaced/compass/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.oddlyspaced.compass.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.oddlyspaced.compass.R
 
@@ -9,7 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(applicationContext, AccountActivity::class.java))
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext, AccountActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
