a7a50a535a2b2f73c0d2cdae4a5739c0ad24201b
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-11 07:07:03 +0530

    EventItem, Global: Add scope integer

--- app/src/main/java/com/oddlyspaced/compass/Global.kt
+++ app/src/main/java/com/oddlyspaced/compass/Global.kt
@@ -7,14 +7,14 @@ class Global {
     companion object {
         fun getMockData() = arrayListOf(
             EventItem(
-                1,
+                1000,
                 "ACM",
                 "https://github.com/oddlyspaced/M-pal/blob/main/media/acm.png?raw=true",
-                1000,
                 "Mini Hacks MUJ",
                 "January 29th",
                 "Online",
                 true,
+                1,
                 arrayListOf("hackathon", "development"),
                 "https://res.cloudinary.com/dashed/image/upload/v1611051427/acm/klgjkuqdehb2g4buvprx.png",
                 "MUJ ACM S-CHAP brings to you a series of mini Hackathons to prep you up for the bigger shows.\nCompete with your team of 4 on 29th, 30th and 31st January and drill your coding skills to come out as best creators!\nHackathon Day - 0 is 29th January. Further information would be communicated to the participants\n++Free for ACM Members++ Non ACM Members: Fee Mentioned\nPrizes are cash and kind. Prize pool may increase!\nLast date to register: 28th January, 2021",
@@ -31,14 +31,14 @@ class Global {
                 )
             ),
             EventItem(
-                2,
+                1001,
                 "IEEE",
                 "https://github.com/oddlyspaced/M-pal/blob/main/media/ieee.png?raw=true",
-                1000,
                 "MUJ Hacks",
                 "April 8th",
                 "Online",
                 true,
+                1,
                 arrayListOf("tag1", "tag2"),
                 "banner link",
                 "IEEE S-CHAP MUJ brings you to the yearly Hackathon \"MUJ HACKS\".Compete with your team of 4 from 8th April to 11th April and drill your coding skills to come out as an innovator.\nThe event is free for all.\nCash prizes upto 1l akh rupees\n \nLast date to register is 7 th April so grab your pillows as you are going have some sleepless nights",
