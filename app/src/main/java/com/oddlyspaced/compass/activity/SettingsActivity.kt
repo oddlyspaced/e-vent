e69e36b6583040a64c1ff9ea827e94629d2809c0
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-11 09:40:05 +0530

    SettingsActivity: Init

---
+++ app/src/main/java/com/oddlyspaced/compass/activity/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.compass.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.compass.R
+
+class SettingsActivity : AppCompatActivity() {e
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
